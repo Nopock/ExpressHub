f01f40d15faaaad3b89ce66d0be03c5e7695bf6d
Nopox-Minecraft <78834703+Nopox-Minecraft@users.noreply.github.com>
2022-05-22 09:33:53 -0700

    Cleaned up the itembuilder consturctor

--- src/main/java/me/nopox/expresshub/utils/Items.java
+++ src/main/java/me/nopox/expresshub/utils/Items.java
@@ -3,17 +3,18 @@ package me.nopox.expresshub.utils;
 import lombok.Getter;
 import me.nopox.expresshub.ExpressHub;
 import me.nopox.expresshub.utils.external.ItemBuilder;
+import org.bukkit.Material;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.inventory.ItemStack;
 
 @Getter
 public class Items {
-    private ExpressHub plugin;
-    private FileConfiguration config;
+    private final ExpressHub plugin;
+    private final FileConfiguration config;
 
     public Items(ExpressHub plugin) {
         this.plugin = plugin;
-        this.config = plugin.getConfig();
+        this.config = this.plugin.getConfig();
     }
 
 
@@ -28,6 +29,10 @@ public class Items {
 
 
     private ItemStack getItem(String id) {
-        return new ItemBuilder(config.getItemStack("Inventory" + id + ".material").getType()).setDisplayName(config.getString("Inventory" + id + ".name")).addLore(config.getStringList("Inventory." + id + ".lore")).setAmount(config.getInt("Inventory." + id + ".amount")).create();
+        return new ItemBuilder(Material.valueOf(config.getString("Inventory." + id + ".material")))
+                .setDisplayName(config.getString("Inventory." + id + ".name"))
+                .addLore(config.getStringList("Inventory." + id + ".lore"))
+                .setAmount(config.getInt("Inventory." + id + ".amount"))
+                .create();
     }
 }
